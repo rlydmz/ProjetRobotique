06f4e802e78de46b4a485e3a30783509313dc497
Raphael Anquetil <reallydrumz@gmail.com>
2018-10-03 13:40:20 +0200

    debut tests moteurs

--- test_moteur.py
+++ test_moteur.py
@@ -23,11 +23,35 @@ dxl_io = pd.DxlIO(ports[0])
 
 
 dxl_io.set_wheel_mode([1])
+dxl_io.set_wheel_mode([2])
 dxl_io.set_moving_speed({1:0})
-
+dxl_io.set_moving_speed({2:0})
 
 # In[28]:
 
 
-dxl_io.scan(range(10))
+#dxl_io.scan(range(10))
 
+
+
+def avance(puissance=10):
+    dxl_io.set_moving_speed({1:-puissance})
+    dxl_io.set_moving_speed({2:puissance})
+
+def recule(puissance=10):
+    dxl_io.set_moving_speed({1:puissance})
+    dxl_io.set_moving_speed({2:-puissance})
+
+def tourne(puissance, angle):
+    signe = signeDe(angle)
+    if angle == 0:
+        avance(puissance)
+    else:
+        dxl_io.set_moving_speed({1:(signe*puissance)})
+        dxl_io.set_moving_speed({2:(signe*puissance)})
+
+def signeDe(nombre):
+    if nombre >= 0:
+        return 1
+    else:
+        return -1
