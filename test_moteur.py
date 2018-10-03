9b65db76c1fe551a714c1603b4d3d39b7cf6b4b7
Raphael Anquetil <reallydrumz@gmail.com>
2018-10-03 22:21:56 +0200

    turn_by

--- test_moteur.py
+++ test_moteur.py
@@ -12,6 +12,8 @@ from sys import exit
 KR = 1.339
 NTS = 60/1.339
 
+ROBOT_WIDTH = 15.3
+
 PERIMETER = 16.3
 RADIUS = PERIMETER/(2*math.pi)
 
@@ -31,17 +33,37 @@ def forward_by(distance, speed): #speed en cm/s
     time.sleep(distance/float(speed))
     stop()
 
+def turn_by(distance, speed): #speed en cm/s
+    print(distance)
+    print(distance/float(speed))
+    value = NTS*speed/PERIMETER
+    dxl_io.set_moving_speed({1:value})
+    dxl_io.set_moving_speed({2:0})
+    time.sleep(distance/float(speed))
+    stop()
+
 def backward(puissance=10):
     dxl_io.set_moving_speed({1:-puissance})
     dxl_io.set_moving_speed({2:puissance})
 
+def backward_by(distance, speed): #speed en cm/s
+    value = NTS*speed/PERIMETER
+    print(value)
+    print(distance/float(speed))
+    backward(value)
+    time.sleep(distance/float(speed))
+    stop()
+
 def turn(angle, puissance=10):
     signe = signeDe(angle)
     if angle == 0:
-        avance(puissance)
+        forward(puissance)
+        time.sleep(0.5)
     else:
-        dxl_io.set_moving_speed({1:(signe*puissance)})
-        dxl_io.set_moving_speed({2:(signe*puissance)})
+        print(angle*2*math.pi/360)
+        wheel_dist = ROBOT_WIDTH*(angle*2*math.pi/360)
+        print(puissance)
+        turn_by(wheel_dist, puissance)
 
 def signeDe(nombre):
     if nombre >= 0:
@@ -71,8 +93,10 @@ dxl_io.set_wheel_mode([1])
 dxl_io.set_wheel_mode([2])
 
 
-forward_by(29.7,10)
-
+#forward_by(24,10)
+#turn(10,10)
+#time.sleep(1)
+#stop()
 
 # In[28]:
 
