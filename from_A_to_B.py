611aad04bab84ad511cdbf38dd82bc36169a3977
LeTuR <38325495+LeTuR@users.noreply.github.com>
2018-10-04 20:34:45 +0200

    Print

--- from_A_to_B.py
+++ from_A_to_B.py
@@ -47,6 +47,7 @@ def move_by (distance, speed):
 
 # Go to B =[x,y,finalAngle]] and speed in cm/s
 def go_to (B, speed=10):
+    print( B[0], B[1], B[2])
     x1, y1, angle1 = B[0], B[1], B[2]
 
     angleToTurn = angle1
