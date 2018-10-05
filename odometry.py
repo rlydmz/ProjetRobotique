b9850f8d5141a42ca0d22e495e3d2e294ef190a3
Raphael Anquetil <reallydrumz@gmail.com>
2018-10-05 09:54:35 +0200

    odometrie OK

--- odometry.py
+++ odometry.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[2]:
-
-
 import pypot.dynamixel as pd
 import time
 import math
@@ -112,53 +106,55 @@ def turn_by (angle, speed=10):
 
 ######################################################################
 
-def odometry(dxl_io):
+def reset_mode(dxl_io):
     dxl_io.set_joint_mode([1])
-    dxl_io.set_joint_mode([2])
+    dxl_io.set_joint_mode([2])    
     dxl_io.set_wheel_mode([1])
     dxl_io.set_wheel_mode([2])
 
+def odometry(dxl_io):
     y1=0
     x1=0
     teta=0
     
     last_time = int(round(time.time() * 1000))
-    
 
     while True:
-        if keyboard.is_pressed('s'):
+        if kb.is_pressed('s'):
             break
-        delta_time = int(round(time.time() * 1000))-last_time
-        movingSpeed1 = dxl_io.get_moving_speed([1])
-        movingSpeed2 = dxl_io.get_moving_speed([2])
+        delta_time = (int(round(time.time() * 1000))-last_time)/float(1000)
+        last_time = int(round(time.time() * 1000))
+        movingSpeed1 = dxl_io.get_present_speed([1])
+        movingSpeed2 = dxl_io.get_present_speed([2])
+        #GAUCHE ID 1 --- DROITE ID 2
         wheelSpeed1 = movingSpeed1[0]*PERIMETER/NTS
         wheelSpeed2 = movingSpeed2[0]*PERIMETER/NTS
-        dw1 = dw1 + wheelSpeed1*delta_time
-        dw2 = dw2 + wheelSpeed2*delta_time
+        wheelSpeed2 *= -1
+        dw1 = wheelSpeed1*delta_time
+        dw2 = wheelSpeed2*delta_time
         if (dw1>dw2):
             dTeta = (dw1-dw2)/ROBOT_WIDTH
             X=(ROBOT_WIDTH*dw2)/(dw1-dw2)
             dy=(X+ROBOT_WIDTH/float(2))*math.sin(dTeta)
-            dx=(X+ROBOT_WIDTH/float(2))*(1-math/cos(dTeta))
-            y1+=dy*math.cos(teta)+math.sin(teta)*dy
-            x1+=-dx*math.sin(teta)+math.cos(teta)*dx
+            dx=(X+ROBOT_WIDTH/float(2))*(1-math.cos(dTeta))
+            y1+=dy*math.cos(teta)-math.sin(teta)*dx
+            x1+=dy*math.sin(teta)+math.cos(teta)*dx
             teta+=dTeta
         elif (dw2>dw1):
             dTeta = (dw2-dw1)/ROBOT_WIDTH
             X=(ROBOT_WIDTH*dw1)/(dw2-dw1)
             dy=(X+ROBOT_WIDTH/float(2))*math.sin(dTeta)
-            dx=(X+ROBOT_WIDTH/float(2))*(1-math/cos(dTeta))
-            y1+=dy*math.cos(teta)+math.sin(teta)*dy
-            x1+=-dx*math.sin(teta)+math.cos(teta)*dx
+            dx=(X+ROBOT_WIDTH/float(2))*(1-math.cos(dTeta))
+            y1+=dy*math.cos(teta)-math.sin(teta)*dx
+            x1+=dy*math.sin(teta)+math.cos(teta)*dx
             teta+=dTeta
         else:
-            y1+=dw1*math.cos(teta)+math.sin(teta)*dw1
-            x1+=dw1*math.cos(teta)-math.sin(teta)*dw1
+            y1+=math.cos(teta)*dw1
+            x1+=dw1*math.sin(teta)
+        print("X = ",x1)
+        print("Y = ",y1)
+        print("ANGLE = ",teta*360/(2*math.pi))
         time.sleep(DELTA_T)
-
-    distance = 0
-    #CALCUL DE LA DISTANCE
-    return distance    
 
 #######################################################################
 def start():
@@ -175,19 +171,6 @@ def start():
 
     return dxl_io
 
-
 dxl_io = start()
 
-distance = odometry(dxl_io)
-
-print(distance)
-
-#forward_by(24,10)
-#turn(10,10)
-#time.sleep(1)
-#stop()
-
-# In[28]:
-
-
-#dxl_io.scan(range(10))
+odometry(dxl_io)
