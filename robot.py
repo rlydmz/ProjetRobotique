dce1610b8e819a1f02ba8f7915e7681650e8780c
Hamza Ezzarouali El Boudri <hapaa@hotmail.fr>
2018-10-05 16:14:52 +0900

    Debug

--- robot.py
+++ robot.py
@@ -14,22 +14,22 @@ def parcours():
     while(True):
         debut=time.time()
         (coordonnes,angleFinal)=cam.noir()
+        print("Coordonnes pointSuivant: "coordonnes)
         pointSuivant=[(coordonnes[1]-320)*4/480,(480-coordonnes[0])*5.5/640,angleFinal]
-        print(coordonnes)
         move.go_to(pointSuivant,vitesse)
         fin=time.time()
         duree=fin-debut
         frequence=1/duree
-        #print(frequence)
-        
-    
-    
+        print("\n\n")
+
+
+
 """
 def goTo(xA,yA,angleDegre):
     #envoie le robot au point de coordonnee (xA,yA) avec un angle de angleDegre
     move.go_to(vitesse,[xA,yA,angleDegre])
 
-    
+
 def isAt():
     #a partir du point (0,0,0), decombien le robot s'est déplacé
 """
